ae94eb826da9152fd0a614ffb3fe3108e1a109ac
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-07-02 12:42:05 +0900

    Initial Commit

---
+++ HelloValidation/HelloValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloValidationApp.swift
+//  HelloValidation
+//
+//  Created by Brandon Jones on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

002dc9e1c64b3cacfa4095fbd50fb73b78789b1c
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-07-02 13:39:35 +0900

    Improved the validation and handled some validation errors.

---
+++ HelloValidation/Errors/LoginFormError.swift
@@ -0,0 +1,15 @@
+//
+//  LoginFormError.swift
+//  HelloValidation
+//
+//  Created by Brandon Jones on 7/2/24.
+//
+
+import Foundation
+
+
+
+struct LoginFormError {
+  var email: String = ""
+  var password: String = ""
+}
--- HelloValidation/LoginView.swift
+++ HelloValidation/LoginView.swift
@@ -7,34 +7,60 @@
 
 import SwiftUI
 
+
+
+
+
+
 struct LoginView: View {
   @State private var email: String = ""
   @State private var password: String = ""
+  @State private var loginFormError = LoginFormError()
+  
+  
+  private func clearForm() {
+    loginFormError = LoginFormError()
+  }
+  
   
   var isFormValid: Bool {
+    clearForm()
+    if email.isEmpty {
+      loginFormError.email = "Email is required!"
+    } else if  !email.isValidEmail {
+      loginFormError.email = "Email is not in correct format!"
+    }
+    if password.isEmpty {
+      loginFormError.password = "Password is required!"
+    }
     return !email.isEmpty && !password.isEmpty && email.isValidEmail
   }
   
   
   var body: some View {
-    VStack {
-      
       Form {
-
         TextField("Email", text: $email)
           .textInputAutocapitalization(.never)
+        if !loginFormError.email.isEmpty {
+          Text(loginFormError.email)
+            .font(.caption)
+        }
         SecureField("Password", text: $password)
+        
+        if !loginFormError.password.isEmpty {
+          Text(loginFormError.password)
+            .font(.caption)
+        }
         Button("Login") {
-          
-        }.disabled(!isFormValid)
+          if isFormValid {
+            //submit the form
+          }
+        }/*.disabled(!isFormValid)*/
       }
-      
-      
-    }
   }
 }
 
 
 #Preview {
-    LoginView()
+  LoginView()
 }
